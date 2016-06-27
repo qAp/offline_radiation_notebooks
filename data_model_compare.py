8b87ae679b8cc347853c1e15a59f71e383e0dad8
Jack Yu <llacque@gmail.com>
2016-06-27 08:49:41 +0800

    gather and store urls for relevant ipython notebooks

---
+++ data_model_compare.py
@@ -0,0 +1,37 @@
+import os
+
+
+
+URL_BASE = os.path.join('http://nbviewer.jupyter.org/github',
+                        'qAp/offline_radiation_notebooks',
+                        'blob/master')
+
+
+# longwave cases
+CASES_LW = [
+    dict(molecules=['H2O'],
+         file_ipynb='longwave_mls_H2O.ipynb'),
+    dict(molecules=['CO2'],
+         file_ipynb='longwave_mls_CO2.ipynb'),
+    dict(molecules=['O3'],
+         file_ipynb='longwave_mls_O3.ipynb'),
+    dict(molecules=['N2O'],
+         file_ipynb='longwave_mls_N2O.ipynb'),
+    dict(molecules=['H2O', 'CO2'],
+         file_ipynb='longwave_mls_H2O_CO2.ipynb'),
+    dict(molecules=['H2O', 'O3'],
+         file_ipynb='longwave_mls_H2O_O3.ipynb'),
+    dict(molecules=['H2O', 'CO2', 'O3'],
+         file_ipynb='longwave_mls_H2O_CO2_O3.ipynb')
+    ]
+
+
+# shortwave cases
+CASES_SW = [
+    dict(molecules=['H2O'],
+         file_ipynb='shortwave_mls_H2O.ipynb'),
+    dict(molecules=['CO2'],
+         file_ipynb='shortwave_mls_CO2.ipynb'),
+    dict(molecules=['O3'],
+         file_ipynb='shortwave_mls_O3.ipynb')]
+
