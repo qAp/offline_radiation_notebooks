6ce5fe90f4cb789631d1c048d694e38f8908515f
Jack Yu <llacque@gmail.com>
2017-09-01 09:40:19 +0800

    H2O band07 mls,saw,trp ng7 nref1 gasc_kdesc
    ------------------------------------------------------------
    commit fc9f69aebfd40492ee3b43877720ee79329c0991
    Author: Jack Yu <llacque@gmail.com>
    Date:   Fri Sep 1 09:14:15 2017 +0800
    
        Correct ig index for output `wldg`
    
        Trivial bug fix.
    ------------------------------------------------------------
    
    [{'atmpro': 'mls',
      'band': 7,
      'commitnumber': 'fc9f69a',
      'klin': 0,
      'molecule': 'h2o',
      'ng_refs': [7],
      'option_wgt_flux': 2,
      'option_wgt_k': 1,
      'ref_pts': [(600, 250)],
      'tsfc': 294,
      'wgt': [(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.9)]},
     {'atmpro': 'saw',
      'band': 7,
      'commitnumber': 'fc9f69a',
      'klin': 0,
      'molecule': 'h2o',
      'ng_refs': [7],
      'option_wgt_flux': 2,
      'option_wgt_k': 1,
      'ref_pts': [(600, 250)],
      'tsfc': 257,
      'wgt': [(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.9)]},
     {'atmpro': 'trp',
      'band': 7,
      'commitnumber': 'fc9f69a',
      'klin': 0,
      'molecule': 'h2o',
      'ng_refs': [7],
      'option_wgt_flux': 2,
      'option_wgt_k': 1,
      'ref_pts': [(600, 250)],
      'tsfc': 300,
      'wgt': [(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.9)]}]

---
+++ longwave/lblnew_20160916/study__g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_none/crd_fc9f69a/atmpro_saw/params.py
@@ -0,0 +1,4 @@
+DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_none/crd_fc9f69a/atmpro_saw'
+DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_none/crd_fc9f69a/atmpro_saw'
+NG_REFS = [7]
+P_REFS = [600]
