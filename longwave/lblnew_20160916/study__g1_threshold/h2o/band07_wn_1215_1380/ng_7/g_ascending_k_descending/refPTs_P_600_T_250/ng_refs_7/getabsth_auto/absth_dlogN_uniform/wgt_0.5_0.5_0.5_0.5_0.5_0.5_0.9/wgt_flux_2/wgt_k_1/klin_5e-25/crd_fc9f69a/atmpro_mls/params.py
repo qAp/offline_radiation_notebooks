fdce76d3ad703b95947c35864b08907003fc19af
Jack Yu <llacque@gmail.com>
2017-09-01 09:23:47 +0800

    H2O band07 mls,saw,trp ng7 nref1 gasc_kdesc
    ------------------------------------------------------------
    commit fc9f69aebfd40492ee3b43877720ee79329c0991
    Author: Jack Yu <llacque@gmail.com>
    Date:   Fri Sep 1 09:14:15 2017 +0800
    
        Correct ig index for output `wldg`
    
        Trivial bug fix.
    ------------------------------------------------------------
    
    vstar = 1215
    
    nband = 165
    
    nv = 200
    
    dv = 0.005
    
    tsfc =294
    
    ng =7
    
    nref =1
    
    p_refs = (/ 600 /)
    
    t_refs = (/ 250 /)
    
    ng_refs = (/ 7 /)
    
    data wgt
         &     /0.5 , 0.5 , 0.5 , 0.5,
         &     0.5 , 0.5 , 0.9/
    
    option_wgt_flux =2
    
    option_wgt_k =1
    
    option_klin =1
    
    klin =5e-25
    
    atmosphere_profiles/mls75.pro
    atmosphere_profiles/saw75.pro
    atmosphere_profiles/trp75.pro

---
+++ longwave/lblnew_20160916/study__g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_5e-25/crd_fc9f69a/atmpro_mls/params.py
@@ -0,0 +1,4 @@
+DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_5e-25/crd_fc9f69a/atmpro_mls'
+DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_5e-25/crd_fc9f69a/atmpro_mls'
+NG_REFS = [7]
+P_REFS = [600]
