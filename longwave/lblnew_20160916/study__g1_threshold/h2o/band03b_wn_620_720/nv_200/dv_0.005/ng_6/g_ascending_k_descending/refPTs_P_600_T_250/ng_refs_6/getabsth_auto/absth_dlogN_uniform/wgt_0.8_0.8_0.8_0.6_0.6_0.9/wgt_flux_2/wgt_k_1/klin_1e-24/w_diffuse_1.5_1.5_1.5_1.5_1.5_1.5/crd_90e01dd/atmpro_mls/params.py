5263e5afc717ff0f503b2da6fd1e4b5a12ee9856
Jack Yu <llacque@gmail.com>
2017-09-26 14:58:04 +0800

    h2o band3b mls ng_refs[6]
    molecule: h2o
    
    band: 3b
    
    dv: 0.005
    
    nv: 200
    
    ref_pts: [(600, 250)]
    
    ng_refs: [6]
    
    option_wgt_flux: 2
    
    option_wgt_k: 1
    
    klin: 1e-24
    
    wgt: [(0.8, 0.8, 0.8, 0.6, 0.6, 0.9)]
    
    atmpro: mls
    
    tsfc: 294
    
    commitnumber: 90e01dd
    
    w_diffuse: [(1.5, 1.5, 1.5, 1.5, 1.5, 1.5)]

---
+++ longwave/lblnew_20160916/study__g1_threshold/h2o/band03b_wn_620_720/nv_200/dv_0.005/ng_6/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_6/getabsth_auto/absth_dlogN_uniform/wgt_0.8_0.8_0.8_0.6_0.6_0.9/wgt_flux_2/wgt_k_1/klin_1e-24/w_diffuse_1.5_1.5_1.5_1.5_1.5_1.5/crd_90e01dd/atmpro_mls/params.py
@@ -0,0 +1,4 @@
+DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band03b_wn_620_720/nv_200/dv_0.005/ng_6/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_6/getabsth_auto/absth_dlogN_uniform/wgt_0.8_0.8_0.8_0.6_0.6_0.9/wgt_flux_2/wgt_k_1/klin_1e-24/w_diffuse_1.5_1.5_1.5_1.5_1.5_1.5/crd_90e01dd/atmpro_mls'
+DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band03b_wn_620_720/nv_200/dv_0.005/ng_6/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_6/getabsth_auto/absth_dlogN_uniform/wgt_0.8_0.8_0.8_0.6_0.6_0.9/wgt_flux_2/wgt_k_1/klin_1e-24/w_diffuse_1.5_1.5_1.5_1.5_1.5_1.5/crd_90e01dd/atmpro_mls'
+NG_REFS = [6]
+P_REFS = [600]
