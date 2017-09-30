4d5f777b1e5743cb4e425156676a8cc6bdc064eb
Jack Yu <llacque@gmail.com>
2017-09-30 11:59:24 +0800

    co2 band3b saw ng_refs[5, 2]
    molecule: co2
    
    band: 3b
    
    dv: 0.005
    
    nv: 200
    
    ref_pts: [(1, 250), (10, 250)]
    
    ng_refs: [5, 2]
    
    option_wgt_flux: 2
    
    option_wgt_k: 1
    
    klin: 0
    
    wgt: [(0, 0.6, 0.5, 0.7, 0.8), (0.8, 0.7)]
    
    w_diffuse: [(1.66, 1.66, 1.66, 1.66, 1.66), (1.66, 1.66)]
    
    commitnumber: 90e01dd
    
    atmpro: saw
    
    tsfc: 257

---
+++ longwave/lblnew_20160916/study__g1_threshold/co2/band03b_wn_620_720/nv_200/dv_0.005/ng_7/g_ascending_k_descending/refPTs_P_1_T_250__P_10_T_250/ng_refs_5__2/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0_0.6_0.5_0.7_0.8__0.8_0.7/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.66_1.66_1.66_1.66_1.66__1.66_1.66/crd_90e01dd/atmpro_saw/params.py
@@ -0,0 +1,4 @@
+DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/co2/band03b_wn_620_720/nv_200/dv_0.005/ng_7/g_ascending_k_descending/refPTs_P_1_T_250__P_10_T_250/ng_refs_5__2/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0_0.6_0.5_0.7_0.8__0.8_0.7/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.66_1.66_1.66_1.66_1.66__1.66_1.66/crd_90e01dd/atmpro_saw'
+DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/co2/band03b_wn_620_720/nv_200/dv_0.005/ng_7/g_ascending_k_descending/refPTs_P_1_T_250__P_10_T_250/ng_refs_5__2/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0_0.6_0.5_0.7_0.8__0.8_0.7/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.66_1.66_1.66_1.66_1.66__1.66_1.66/crd_90e01dd/atmpro_saw'
+NG_REFS = [5, 2]
+P_REFS = [1, 10]
