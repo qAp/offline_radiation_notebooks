d3f54c40147673238aa5732092570a7432d858ab
Jack Yu <llacque@gmail.com>
2017-12-01 15:54:04 +0800

    ['h2o', 'co2'] band3b mls
    molecule: ['h2o', 'co2']
    
    band: 3b
    
    atmpro: mls
    
    tsfc: 294
    
    nv: 200
    
    dv: 0.005
    
    commitnumber: cace93a

---
+++ longwave/lblnew_20160916/study__g1_threshold/h2o_1_co2_1_o3_0_n2o_0_ch4_0_o2_0/band03b_wn_620_720/nv_200/dv_0.005/crd_cace93a/atmpro_mls/param.py
@@ -0,0 +1,2 @@
+DIR_FORTRAN = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o_1_co2_1_o3_0_n2o_0_ch4_0_o2_0/band03b_wn_620_720/nv_200/dv_0.005/crd_cace93a/atmpro_mls'
+PARAM = {'molecule': ['h2o', 'co2'], 'band': '3b', 'atmpro': 'mls', 'tsfc': 294, 'nv': 200, 'dv': 0.005, 'commitnumber': 'cace93a'}
