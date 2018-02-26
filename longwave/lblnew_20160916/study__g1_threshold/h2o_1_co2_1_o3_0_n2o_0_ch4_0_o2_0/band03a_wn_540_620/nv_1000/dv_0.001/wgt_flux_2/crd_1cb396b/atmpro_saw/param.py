dcf90a2b0f5e4c988f30a9a4b8c79109507d2b3a
Jack Yu <llacque@gmail.com>
2018-02-26 21:25:48 +0800

    ['h2o', 'co2'] band3a saw
    molecule: ['h2o', 'co2']
    
    band: 3a
    
    atmpro: saw
    
    tsfc: 257
    
    nv: 1000
    
    dv: 0.001
    
    option_wgt_flux: 2
    
    commitnumber: 1cb396b

---
+++ longwave/lblnew_20160916/study__g1_threshold/h2o_1_co2_1_o3_0_n2o_0_ch4_0_o2_0/band03a_wn_540_620/nv_1000/dv_0.001/wgt_flux_2/crd_1cb396b/atmpro_saw/param.py
@@ -0,0 +1,2 @@
+DIR_FORTRAN = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o_1_co2_1_o3_0_n2o_0_ch4_0_o2_0/band03a_wn_540_620/nv_1000/dv_0.001/wgt_flux_2/crd_1cb396b/atmpro_saw'
+PARAM = {'molecule': ['h2o', 'co2'], 'band': '3a', 'atmpro': 'saw', 'tsfc': 257, 'nv': 1000, 'dv': 0.001, 'option_wgt_flux': 2, 'commitnumber': '1cb396b'}
