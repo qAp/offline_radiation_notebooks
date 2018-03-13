7502665cda562abe3eb0aff064e1f6eaa022f40f
Jack Yu <llacque@gmail.com>
2018-03-13 16:46:35 +0800

    Test cliradlw analysis notebook

---
+++ longwave/lblnew_20160916/clirad/co2_-9012_n2o_100.45_ch4_101/band_1_4_6/atmpro_trp/cliradlw_4kfj23/param.py
@@ -0,0 +1,2 @@
+DIR_FORTRAN = '/chia_cluster/home/jackyu/radiation/clirad-lw/LW/examples/lblnew/co2_-9012_n2o_100.45_ch4_101/band_1_4_6/atmpro_trp/cliradlw_4kfj23'
+PARAM = {'commitnumber': '4kfj23', 'molecule': {'co2': -9012, 'n2o': 100.45, 'ch4': 101}, 'band': [4, 6, 1], 'atmpro': 'trp', 'tsfc': 294.01255}
