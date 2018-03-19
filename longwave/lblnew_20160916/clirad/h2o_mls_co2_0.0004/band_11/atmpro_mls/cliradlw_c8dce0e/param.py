d419e526f4c7e41fbd5c9ea5d865f58326f59696
Jack Yu <llacque@gmail.com>
2018-03-19 16:57:34 +0800

    clirad-lw h2o+co2 band11 test

---
+++ longwave/lblnew_20160916/clirad/h2o_mls_co2_0.0004/band_11/atmpro_mls/cliradlw_c8dce0e/param.py
@@ -0,0 +1,3 @@
+DIR_FORTRAN = '/chia_cluster/home/jackyu/radiation/clirad-lw/LW/examples/lblnew/h2o_mls_co2_0.0004/band_11/atmpro_mls/cliradlw_c8dce0e'
+PARAM = {'atmpro': 'mls', 'band': [11], 'commitnumber': 'c8dce0e', 'molecule': {'h2o': 'atmpro', 'co2': 0.0004}, 'tsfc': 294}
+DIR_FORTRAN_LBLNEW = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o_1_co2_1_o3_0_n2o_0_ch4_0_o2_0/band09_wn_1900_3000/nv_1000/dv_0.001/crd_8ba2806/atmpro_mls'
