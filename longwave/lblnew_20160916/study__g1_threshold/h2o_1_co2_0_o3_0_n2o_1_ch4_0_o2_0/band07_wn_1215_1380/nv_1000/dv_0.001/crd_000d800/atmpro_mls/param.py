31ebe2f44bc706a91a5b7461f67ef415b2f2063a
Jack Yu <llacque@gmail.com>
2018-04-10 13:02:37 +0800

    {'h2o': 'atmpro', 'n2o': 3.2e-07} band7 mls
    commitnumber: 000d800
    
    molecule: {'h2o': 'atmpro', 'n2o': 3.2e-07}
    
    band: 7
    
    nv: 1000
    
    dv: 0.001
    
    atmpro: mls
    
    tsfc: 294

---
+++ longwave/lblnew_20160916/study__g1_threshold/h2o_1_co2_0_o3_0_n2o_1_ch4_0_o2_0/band07_wn_1215_1380/nv_1000/dv_0.001/crd_000d800/atmpro_mls/param.py
@@ -0,0 +1,2 @@
+DIR_FORTRAN = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o_1_co2_0_o3_0_n2o_1_ch4_0_o2_0/band07_wn_1215_1380/nv_1000/dv_0.001/crd_000d800/atmpro_mls'
+PARAM = {'commitnumber': '000d800', 'molecule': {'h2o': 'atmpro', 'n2o': 3.2e-07}, 'band': '7', 'nv': 1000, 'dv': 0.001, 'atmpro': 'mls', 'tsfc': 294}
