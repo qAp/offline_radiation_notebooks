4f3db6eff02ff76d5f78ccf99314f62d20fcd770
Jack Yu <llacque@gmail.com>
2018-04-20 14:41:12 +0800

    {'o3': 'atmpro'} band[7] mls
    atmpro: mls
    
    band: [7]
    
    commitnumber: 4f33c91
    
    molecule: {'o3': 'atmpro'}
    
    tsfc: 294

---
+++ longwave/lblnew_20160916/clirad/o3_mls/band_7/atmpro_mls/cliradlw_4f33c91/param.py
@@ -0,0 +1,2 @@
+PARAM = {'atmpro': 'mls', 'band': [7], 'commitnumber': '4f33c91', 'molecule': {'o3': 'atmpro'}, 'tsfc': 294}
+PARAM_LBLNEW = {'molecule': 'o3', 'band': '5', 'commitnumber': 'd62b618', 'vmin': 980, 'vmax': 1100, 'dv': 0.001, 'nv': 1000, 'ref_pts': [(1, 250), (50, 250)], 'ng_refs': [2, 5], 'ng_adju': [0, 0], 'klin': 2e-20, 'option_wgt_k': 1, 'wgt': [(0.35, 0.6), (0.5, 0.55, 0.7, 0.9, 1.0)], 'w_diffuse': [(1.6, 1.75), (1.55, 1.66, 1.7, 1.75, 1.8)], 'option_wgt_flux': 1, 'atmpro': 'mls', 'tsfc': 294, 'conc': None, 'option_compute_btable': 0, 'option_compute_ktable': 1}
