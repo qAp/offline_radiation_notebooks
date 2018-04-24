5103d03ce5be3fd6f8e175013a275446a2c83687
Jack Yu <llacque@gmail.com>
2018-04-24 16:15:29 +0800

    clirad-lw {'h2o': 'atmpro'} band[5] mls
    atmpro: mls
    
    band: [5]
    
    commitnumber: 7a7a0e1
    
    molecule: {'h2o': 'atmpro'}
    
    tsfc: 294

---
+++ longwave/lblnew_20160916/clirad/h2o_mls/band_5/atmpro_mls/cliradlw_7a7a0e1/param.py
@@ -0,0 +1,2 @@
+PARAM = {'atmpro': 'mls', 'band': [5], 'commitnumber': '7a7a0e1', 'molecule': {'h2o': 'atmpro'}, 'tsfc': 294}
+PARAM_LBLNEW = {'molecule': 'h2o', 'band': '3c', 'commitnumber': 'd62b618', 'vmin': 720, 'vmax': 800, 'dv': 0.001, 'nv': 1000, 'ref_pts': [(600, 250)], 'ng_refs': [5], 'ng_adju': [0], 'klin': 5e-25, 'option_wgt_k': 1, 'wgt': [(0.5, 0.5, 0.6, 0.7, 0.9)], 'w_diffuse': [(1.55, 1.6, 1.66, 1.66, 1.8)], 'option_wgt_flux': 1, 'atmpro': 'mls', 'tsfc': 294, 'conc': None, 'option_compute_btable': 0, 'option_compute_ktable': 1}
