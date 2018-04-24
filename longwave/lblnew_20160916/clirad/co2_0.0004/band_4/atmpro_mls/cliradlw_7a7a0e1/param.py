ec3bf6e1bdb3080424995a6f6d3623133e3dfa8f
Jack Yu <llacque@gmail.com>
2018-04-24 16:15:06 +0800

    clirad-lw {'co2': 0.0004} band[4] mls
    atmpro: mls
    
    band: [4]
    
    commitnumber: 7a7a0e1
    
    molecule: {'co2': 0.0004}
    
    tsfc: 294

---
+++ longwave/lblnew_20160916/clirad/co2_0.0004/band_4/atmpro_mls/cliradlw_7a7a0e1/param.py
@@ -0,0 +1,2 @@
+PARAM = {'atmpro': 'mls', 'band': [4], 'commitnumber': '7a7a0e1', 'molecule': {'co2': 0.0004}, 'tsfc': 294}
+PARAM_LBLNEW = {'molecule': 'co2', 'band': '3b', 'commitnumber': 'd62b618', 'vmin': 620, 'vmax': 720, 'dv': 0.001, 'nv': 1000, 'ref_pts': [(1, 250), (10, 250)], 'ng_refs': [5, 2], 'ng_adju': [0, 0], 'klin': 0, 'option_wgt_k': 1, 'wgt': [(0, 0.6, 0.5, 0.7, 0.8), (0.9, 0.6)], 'w_diffuse': [(1.66, 1.66, 1.66, 1.66, 1.66), (1.66, 1.66)], 'option_wgt_flux': 1, 'atmpro': 'trp', 'tsfc': 294, 'conc': 0.0004, 'option_compute_btable': 0, 'option_compute_ktable': 1}
