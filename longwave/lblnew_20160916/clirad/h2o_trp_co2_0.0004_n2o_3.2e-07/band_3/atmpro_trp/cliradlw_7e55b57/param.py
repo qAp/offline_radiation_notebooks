94a1b56ae829a773bc68d78dc98e9ab569b93d74
Jack Yu <llacque@gmail.com>
2018-05-25 15:08:32 +0800

    clirad-lw {'co2': 0.0004, 'h2o': 'atmpro', 'n2o': 3.2e-07} band[3] trp
    atmpro: trp
    
    band: [3]
    
    commitnumber: 7e55b57
    
    molecule: {'co2': 0.0004, 'h2o': 'atmpro', 'n2o': 3.2e-07}
    
    tsfc: 300

---
+++ longwave/lblnew_20160916/clirad/h2o_trp_co2_0.0004_n2o_3.2e-07/band_3/atmpro_trp/cliradlw_7e55b57/param.py
@@ -0,0 +1,2 @@
+PARAM = {'atmpro': 'trp', 'band': [3], 'commitnumber': '7e55b57', 'molecule': {'co2': 0.0004, 'h2o': 'atmpro', 'n2o': 3.2e-07}, 'tsfc': 300}
+PARAM_LBLNEW = {'atmpro': 'trp', 'band': '3a', 'commitnumber': '5014a19', 'dv': 0.01, 'molecule': {'co2': 0.0004, 'h2o': 'atmpro', 'n2o': 3.2e-07}, 'nv': 100, 'tsfc': 300}
