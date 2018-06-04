b2f4480fd451662daaa82ec9492b63434fa31672
Jack Yu <llacque@gmail.com>
2018-06-04 17:50:27 +0800

    Repo cleanup: h2o band07

--- longwave/lblnew_20160916/study__g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.9_1.66_1.66_1.66_1.66_1.66_1.8/crd_bd5b4a5/atmpro_trp/params.py
+++
@@ -1,4 +0,0 @@
-DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.9_1.66_1.66_1.66_1.66_1.66_1.8/crd_bd5b4a5/atmpro_trp'
-DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.9_1.66_1.66_1.66_1.66_1.66_1.8/crd_bd5b4a5/atmpro_trp'
-NG_REFS = [7]
-P_REFS = [600]
--- longwave/lblnew_20160916/study__g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_2_1.6_1.6_1.6_1.6_1.6_1.8/crd_bd5b4a5/atmpro_saw/params.py
+++
@@ -1,4 +0,0 @@
-DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_2_1.6_1.6_1.6_1.6_1.6_1.8/crd_bd5b4a5/atmpro_saw'
-DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band07_wn_1215_1380/ng_7/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_7/getabsth_auto/absth_dlogN_uniform/wgt_0.5_0.5_0.5_0.5_0.5_0.5_0.9/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_2_1.6_1.6_1.6_1.6_1.6_1.8/crd_bd5b4a5/atmpro_saw'
-NG_REFS = [7]
-P_REFS = [600]
