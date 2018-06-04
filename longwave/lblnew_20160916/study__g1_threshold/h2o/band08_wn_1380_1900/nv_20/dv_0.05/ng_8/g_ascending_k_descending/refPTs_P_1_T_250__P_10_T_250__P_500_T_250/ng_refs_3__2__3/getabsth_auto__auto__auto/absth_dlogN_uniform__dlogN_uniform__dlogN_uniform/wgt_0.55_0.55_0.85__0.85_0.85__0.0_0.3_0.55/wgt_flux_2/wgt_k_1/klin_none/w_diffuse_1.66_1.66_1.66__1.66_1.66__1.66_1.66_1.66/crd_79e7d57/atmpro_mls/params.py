3e8eeada0c2cbda3718364a8260d8d2cb1f6d026
Jack Yu <llacque@gmail.com>
2018-06-04 17:56:02 +0800

    Repo cleanup: h2o band08

--- longwave/lblnew_20160916/study__g1_threshold/h2o/band08_wn_1380_1900/nv_20/dv_0.05/ng_8/g_ascending_k_descending/refPTs_P_1_T_250__P_10_T_250__P_500_T_250/ng_refs_3__2__3/getabsth_auto__auto__auto/absth_dlogN_uniform__dlogN_uniform__dlogN_uniform/wgt_0.55_0.55_0.85__0.85_0.85__0.0_0.3_0.55/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.66_1.66_1.66__1.66_1.66__1.66_1.66_1.66/crd_79e7d57/atmpro_mls/params.py
+++
@@ -1,4 +0,0 @@
-DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band08_wn_1380_1900/nv_20/dv_0.05/ng_8/g_ascending_k_descending/refPTs_P_1_T_250__P_10_T_250__P_500_T_250/ng_refs_3__2__3/getabsth_auto__auto__auto/absth_dlogN_uniform__dlogN_uniform__dlogN_uniform/wgt_0.55_0.55_0.85__0.85_0.85__0.0_0.3_0.55/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.66_1.66_1.66__1.66_1.66__1.66_1.66_1.66/crd_79e7d57/atmpro_mls'
-DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band08_wn_1380_1900/nv_20/dv_0.05/ng_8/g_ascending_k_descending/refPTs_P_1_T_250__P_10_T_250__P_500_T_250/ng_refs_3__2__3/getabsth_auto__auto__auto/absth_dlogN_uniform__dlogN_uniform__dlogN_uniform/wgt_0.55_0.55_0.85__0.85_0.85__0.0_0.3_0.55/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.66_1.66_1.66__1.66_1.66__1.66_1.66_1.66/crd_79e7d57/atmpro_mls'
-NG_REFS = [3, 2, 3]
-P_REFS = [1, 10, 500]
