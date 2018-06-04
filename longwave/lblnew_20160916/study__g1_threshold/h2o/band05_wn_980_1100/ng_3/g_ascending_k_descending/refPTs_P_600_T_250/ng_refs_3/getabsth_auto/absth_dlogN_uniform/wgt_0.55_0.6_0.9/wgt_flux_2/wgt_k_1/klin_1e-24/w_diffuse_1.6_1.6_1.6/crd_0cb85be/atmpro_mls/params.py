7b70be1f3496f522f9d40ea1aed52f7a1d9baf9d
Jack Yu <llacque@gmail.com>
2018-06-04 17:38:25 +0800

    Repo cleanup: h2o band05

--- longwave/lblnew_20160916/study__g1_threshold/h2o/band05_wn_980_1100/ng_3/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_3/getabsth_auto/absth_dlogN_uniform/wgt_0.55_0.6_0.9/wgt_flux_2/wgt_k_1/klin_1e-24/w_diffuse_1.6_1.6_1.6/crd_0cb85be/atmpro_mls/params.py
+++
@@ -1,4 +0,0 @@
-DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band05_wn_980_1100/ng_3/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_3/getabsth_auto/absth_dlogN_uniform/wgt_0.55_0.6_0.9/wgt_flux_2/wgt_k_1/klin_1e-24/w_diffuse_1.6_1.6_1.6/crd_0cb85be/atmpro_mls'
-DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band05_wn_980_1100/ng_3/g_ascending_k_descending/refPTs_P_600_T_250/ng_refs_3/getabsth_auto/absth_dlogN_uniform/wgt_0.55_0.6_0.9/wgt_flux_2/wgt_k_1/klin_1e-24/w_diffuse_1.6_1.6_1.6/crd_0cb85be/atmpro_mls'
-NG_REFS = [3]
-P_REFS = [600]
--- longwave/lblnew_20160916/study__g1_threshold/h2o/band05_wn_980_1100/ng_4/g_ascending_k_descending/refPTs_P_50_T_250/ng_refs_4/getabsth_auto/absth_dlogN_uniform/wgt_1_0.6_0.4_0.3/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.6_1.6_1.6_1.6/crd_0cb85be/atmpro_saw/params.py
+++
@@ -1,4 +0,0 @@
-DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band05_wn_980_1100/ng_4/g_ascending_k_descending/refPTs_P_50_T_250/ng_refs_4/getabsth_auto/absth_dlogN_uniform/wgt_1_0.6_0.4_0.3/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.6_1.6_1.6_1.6/crd_0cb85be/atmpro_saw'
-DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o/band05_wn_980_1100/ng_4/g_ascending_k_descending/refPTs_P_50_T_250/ng_refs_4/getabsth_auto/absth_dlogN_uniform/wgt_1_0.6_0.4_0.3/wgt_flux_2/wgt_k_1/klin_none/w_diffuse_1.6_1.6_1.6_1.6/crd_0cb85be/atmpro_saw'
-NG_REFS = [4]
-P_REFS = [50]
