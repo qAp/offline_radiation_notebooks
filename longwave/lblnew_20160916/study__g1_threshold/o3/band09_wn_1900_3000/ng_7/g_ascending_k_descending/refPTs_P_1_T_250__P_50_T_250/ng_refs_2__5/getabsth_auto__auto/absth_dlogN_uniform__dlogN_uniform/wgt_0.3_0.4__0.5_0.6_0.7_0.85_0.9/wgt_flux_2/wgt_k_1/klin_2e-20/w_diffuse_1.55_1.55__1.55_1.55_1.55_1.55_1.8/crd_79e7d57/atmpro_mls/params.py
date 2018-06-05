7cfce9f45c2236bd3397def7f26d3c84b5f16c5b
Jack Yu <llacque@gmail.com>
2018-06-05 09:38:58 +0800

    Repo cleanup: o3 band09

--- longwave/lblnew_20160916/study__g1_threshold/o3/band09_wn_1900_3000/ng_7/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_2__5/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.3_0.4__0.5_0.6_0.7_0.85_0.9/wgt_flux_2/wgt_k_1/klin_2e-20/w_diffuse_1.55_1.55__1.55_1.55_1.55_1.55_1.8/crd_79e7d57/atmpro_mls/params.py
+++
@@ -1,4 +0,0 @@
-DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/o3/band09_wn_1900_3000/ng_7/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_2__5/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.3_0.4__0.5_0.6_0.7_0.85_0.9/wgt_flux_2/wgt_k_1/klin_2e-20/w_diffuse_1.55_1.55__1.55_1.55_1.55_1.55_1.8/crd_79e7d57/atmpro_mls'
-DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/o3/band09_wn_1900_3000/ng_7/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_2__5/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.3_0.4__0.5_0.6_0.7_0.85_0.9/wgt_flux_2/wgt_k_1/klin_2e-20/w_diffuse_1.55_1.55__1.55_1.55_1.55_1.55_1.8/crd_79e7d57/atmpro_mls'
-NG_REFS = [2, 5]
-P_REFS = [1, 50]
--- longwave/lblnew_20160916/study__g1_threshold/o3/band09_wn_1900_3000/nv_200/dv_0.005/ng_7/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_2__5/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.3_0.4__0.5_0.6_0.7_0.85_0.9/wgt_flux_2/wgt_k_1/klin_2e-20/w_diffuse_1.55_1.55__1.55_1.55_1.55_1.55_1.8/crd_9d81085/atmpro_saw/params.py
+++
@@ -1,4 +0,0 @@
-DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/o3/band09_wn_1900_3000/nv_200/dv_0.005/ng_7/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_2__5/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.3_0.4__0.5_0.6_0.7_0.85_0.9/wgt_flux_2/wgt_k_1/klin_2e-20/w_diffuse_1.55_1.55__1.55_1.55_1.55_1.55_1.8/crd_9d81085/atmpro_saw'
-DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/o3/band09_wn_1900_3000/nv_200/dv_0.005/ng_7/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_2__5/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.3_0.4__0.5_0.6_0.7_0.85_0.9/wgt_flux_2/wgt_k_1/klin_2e-20/w_diffuse_1.55_1.55__1.55_1.55_1.55_1.55_1.8/crd_9d81085/atmpro_saw'
-NG_REFS = [2, 5]
-P_REFS = [1, 50]
