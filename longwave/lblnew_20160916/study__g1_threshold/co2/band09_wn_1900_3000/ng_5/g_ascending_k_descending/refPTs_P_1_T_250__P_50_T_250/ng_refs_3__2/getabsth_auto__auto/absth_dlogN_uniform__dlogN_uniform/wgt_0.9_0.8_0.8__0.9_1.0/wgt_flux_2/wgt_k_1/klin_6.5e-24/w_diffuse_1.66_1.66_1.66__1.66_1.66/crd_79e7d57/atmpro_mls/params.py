b26feb2d69bc09765514a3dad75fcd444e5b9884
Jack Yu <llacque@gmail.com>
2018-06-05 09:28:48 +0800

    Repo cleanup: co2 band09

--- longwave/lblnew_20160916/study__g1_threshold/co2/band09_wn_1900_3000/ng_5/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_3__2/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.9_0.8_0.8__0.9_1.0/wgt_flux_2/wgt_k_1/klin_6.5e-24/w_diffuse_1.66_1.66_1.66__1.66_1.66/crd_79e7d57/atmpro_mls/params.py
+++
@@ -1,4 +0,0 @@
-DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/co2/band09_wn_1900_3000/ng_5/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_3__2/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.9_0.8_0.8__0.9_1.0/wgt_flux_2/wgt_k_1/klin_6.5e-24/w_diffuse_1.66_1.66_1.66__1.66_1.66/crd_79e7d57/atmpro_mls'
-DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/co2/band09_wn_1900_3000/ng_5/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_3__2/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.9_0.8_0.8__0.9_1.0/wgt_flux_2/wgt_k_1/klin_6.5e-24/w_diffuse_1.66_1.66_1.66__1.66_1.66/crd_79e7d57/atmpro_mls'
-NG_REFS = [3, 2]
-P_REFS = [1, 50]
--- longwave/lblnew_20160916/study__g1_threshold/co2/band09_wn_1900_3000/ng_6/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_3__3/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.6_0.6_0.6__0.6_0.7_0.85/wgt_flux_2/wgt_k_1/klin_6.5e-24/w_diffuse_1.66_1.66_1.66__1.66_1.66_1.8/crd_79e7d57/atmpro_mls/params.py
+++
@@ -1,4 +0,0 @@
-DIR_CRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/co2/band09_wn_1900_3000/ng_6/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_3__3/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.6_0.6_0.6__0.6_0.7_0.85/wgt_flux_2/wgt_k_1/klin_6.5e-24/w_diffuse_1.66_1.66_1.66__1.66_1.66_1.8/crd_79e7d57/atmpro_mls'
-DIR_XCRD = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/co2/band09_wn_1900_3000/ng_6/g_ascending_k_descending/refPTs_P_1_T_250__P_50_T_250/ng_refs_3__3/getabsth_auto__auto/absth_dlogN_uniform__dlogN_uniform/wgt_0.6_0.6_0.6__0.6_0.7_0.85/wgt_flux_2/wgt_k_1/klin_6.5e-24/w_diffuse_1.66_1.66_1.66__1.66_1.66_1.8/crd_79e7d57/atmpro_mls'
-NG_REFS = [3, 3]
-P_REFS = [1, 50]
